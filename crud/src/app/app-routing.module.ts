e199d6f1189e0068e020e77cd0dfd298a2c08b97
Aleksandar Iilijevski <aleksandar.ilijevski@beelivio.in>
2023-12-12 13:40:41 +0100

    adding buttons

--- crud/src/app/app-routing.module.ts
+++ crud/src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainBodyComponent } from './components/main-body/main-body.component';
+import { MyMarathonsComponent } from './components/my-marathons/my-marathons.component';
+import { MarathonsComponent } from './components/marathons/marathons.component';
 
 // Define your routes here
 const routes: Routes = [
     { path: 'home', component: MainBodyComponent },
+    { path: 'mymarathons', component: MyMarathonsComponent },
+    { path: 'marathons', component: MarathonsComponent },
     { path: '**', redirectTo: 'home', pathMatch: 'full' },
-    // { path: 'mymarathons', component: MyMarathonsComponent },
-    // { path: 'marathons', component: MarathonsComponent },
 ];
 
 @NgModule({
--- crud/src/app/app.module.ts
+++ crud/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {MatCardModule} from '@angular/material/card'
+import {MatButtonModule} from '@angular/material/button';
 
 import { MarathonsService, UserService } from './data.services';
 import { HttpClientModule } from '@angular/common/http';
@@ -14,6 +15,9 @@ import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { MainBodyComponent } from './components/main-body/main-body.component';
 import { AppRoutingModule } from './app-routing.module';
+import { FeaturedCardComponent } from './components/featured-card/featured-card.component';
+import { MyMarathonsComponent } from './components/my-marathons/my-marathons.component';
+import { MarathonsComponent } from './components/marathons/marathons.component';
 
 @NgModule({
   declarations: [
@@ -21,12 +25,16 @@ import { AppRoutingModule } from './app-routing.module';
     HomeComponent,
     HeaderComponent,
     FooterComponent,
-    MainBodyComponent
+    MainBodyComponent,
+    FeaturedCardComponent,
+    MyMarathonsComponent,
+    MarathonsComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     MatCardModule,
+    MatButtonModule,
     HttpClientInMemoryWebApiModule.forRoot(MarathonsService),
     HttpClientInMemoryWebApiModule.forRoot(UserService),
     BrowserAnimationsModule,
