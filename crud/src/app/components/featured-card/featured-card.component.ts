8018f11a54580e1f9d7fd6ee05dc03c05e83b4da
Aleksandar Iilijevski <aleksandar.ilijevski@beelivio.in>
2023-12-12 16:51:11 +0100

    adding logic

--- crud/src/app/components/featured-card/featured-card.component.ts
+++ crud/src/app/components/featured-card/featured-card.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Marathon, MarathonsDb } from 'src/app/models/marathon.interface';
 
 @Component({
   selector: 'app-featured-card',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./featured-card.component.css']
 })
 export class FeaturedCardComponent {
+  @Input() marathons: MarathonsDb = {} as MarathonsDb;
 
-}
+  getFutureMarathons(marathons: MarathonsDb): MarathonsDb {
+    const dateNow = new Date().getTime();
+    const futureMarathons = {marathons: [] as Marathon[]};
+
+    for (const marathon of marathons.marathons) {
+      const dateMarathon = new Date(marathon.date).getTime();
+      if (dateNow <= dateMarathon) {
+        futureMarathons.marathons.push(marathon)};
+      }
+      return futureMarathons;
+    }
+
+  }
--- crud/src/app/components/main-body/main-body.component.ts
+++ crud/src/app/components/main-body/main-body.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { MarathonsService, UserService } from '../../data.services';
+import { MarathonsDb } from 'src/app/models/marathon.interface';
+import { UsersDb } from 'src/app/models/user.intereface';
 
 @Component({
   selector: 'app-main-body',
@@ -6,5 +9,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./main-body.component.css']
 })
 export class MainBodyComponent {
+  constructor(private userService: UserService, private marathonsService: MarathonsService) { };
 
+  marathons: MarathonsDb = this.marathonsService.createDb();
+  users: UsersDb = this.userService.createDb();
+
+  
+  
 }
