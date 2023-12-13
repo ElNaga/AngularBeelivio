47ca7cff86e209e36aaef7152aa18355edb863e4
Aleksandar Ilijevski <aleksandar.ilijevski.feit@gmail.com>
2023-12-13 21:38:13 +0100

    adding pipe

---
+++ crud/src/app/pipes/filter.pipe.ts
@@ -0,0 +1,31 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { Marathon } from '../models/marathon.interface';
+
+@Pipe({
+  name: 'filter'
+})
+export class FilterPipe implements PipeTransform {
+
+  // transform(shto: tipShto, filterString: tipString): outputTip{
+  // if (!items) return [];
+  // if (!searchTerm) return items;
+
+  // filterString = this.filterString.toLowerCase();
+  // return shto.filter(shto => {
+  // return shto.name.tolowercase().icludes(filterString)
+  // })
+  // }
+
+  // Magijata e transform() + angular Pipe library shto kje mi ovozmozhi da si go koristam direkt pipe-ot vo komponenta so dekorator @Pipe({name: KAKO_SE_VIKA_PIPE-ot})
+
+  transform(marathons: Marathon[], searchString: string): Marathon[] {
+    if (!marathons) return [];
+    if (!searchString) return marathons;
+
+    searchString = searchString.toLowerCase();
+    return marathons.filter(marathon => {
+      return marathon.name.toLowerCase().includes(searchString);
+    })
+  }
+
+}
