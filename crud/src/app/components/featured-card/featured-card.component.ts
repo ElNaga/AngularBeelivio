333d9dc3854b8224e40e492e5a57d908abe31cd5
Aleksandar Iilijevski <aleksandar.ilijevski@beelivio.in>
2023-12-13 16:50:00 +0100

    latest changes search

--- crud/src/app/components/featured-card/featured-card.component.ts
+++ crud/src/app/components/featured-card/featured-card.component.ts
@@ -10,12 +10,19 @@ import { Marathon, MarathonsDb } from 'src/app/models/marathon.interface';
 export class FeaturedCardComponent implements OnInit {
   @Input() futureMarathons: Marathon[] = [] as Marathon[];
 
+  searchString: string = 'Prilep';
+  filterdMarathons = this.futureMarathons.filter(marathon => marathon.name.includes(this.searchString))
+
   color: ThemePalette = 'accent';
   checked = false;
   disabled = false;
 
+  printInput (string: string): void {
+    console.log(string);
+  }
   
   async ngOnInit() {
+    console.log( 'this is from future card',this.futureMarathons)
     console.log( 'this is from future card',this.futureMarathons)
   }
 }
